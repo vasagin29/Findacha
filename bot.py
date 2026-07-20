2b6a7020441ed23a3fee419fb32de75661058cad
vasagin29 <vasagin29@gmail.com>
2026-07-20 13:13:33 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from telegram import Bot
 
+
 TOKEN = os.environ["TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
@@ -26,7 +27,9 @@ def save_seen(seen):
 
 
 async def check_ss():
+
     bot = Bot(token=TOKEN)
+
     seen = load_seen()
 
     headers = {
@@ -39,47 +42,60 @@ async def check_ss():
             headers=headers,
             timeout=15
         )
+
         response.raise_for_status()
 
     except Exception as e:
         print("Ошибка загрузки SS.lv:", e)
         return
 
-    soup = BeautifulSoup(response.text, "html.parser")
 
     print("HTML размер:", len(response.text))
+
+    soup = BeautifulSoup(response.text, "html.parser")
 
     links = []
 
     for a in soup.find_all("a", href=True):
+
         href = a["href"]
 
         if "/msg/" in href:
+
             if href.startswith("/"):
                 href = "https://www.ss.lv" + href
 
             if href not in links:
                 links.append(href)
 
+
+    print("Найдено ссылок:", len(links))
+    print(links[:5])
+
+
     new_links = [
         link for link in links
         if link not in seen
     ]
 
-    print("Найдено ссылок:", len(links))
-    print(links[:5])
 
     if new_links:
+
         text = "🏡 Новые дачи SS.lv:\n\n"
 
         for link in new_links[:5]:
             text += link + "\n\n"
-            seen.add(link)
+
 
         await bot.send_message(
             chat_id=CHAT_ID,
             text=text
         )
+
+
+        for link in new_links[:5]:
+            seen.add(link)
+
 
         save_seen(seen)
 
